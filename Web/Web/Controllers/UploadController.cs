e96565e3db7d10fd99654d0eed4a85e7ae58db7b
Jacob <jacob.larsen6401@hotmail.dk>
2021-03-17 16:05:31 +0100

    First

---
+++ Web/Web/Controllers/UploadController.cs
@@ -0,0 +1,38 @@
+﻿using BookingWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookingWeb.Service;
+
+namespace BookingWeb.Controllers
+{
+    public class UploadController : Controller
+    {
+        [HttpGet]
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(File file)
+        {
+            string message = "";
+
+            if (ModelState.IsValid)
+            {
+                var rpcClient = new UploadService();
+                await rpcClient.CallAsync(file);
+
+                rpcClient.Close();
+            }
+            else
+            {
+                message = "Failed to create the product. Please try again";
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Web/Web/Service/UploadService.cs
@@ -0,0 +1,76 @@
+﻿using BookingWeb.Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookingWeb.Service
+{
+    public class UploadService
+    {
+        private const string QUEUE_NAME = "rpc_queue";
+
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string replyQueueName;
+        private readonly EventingBasicConsumer consumer;
+        private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> callbackMapper =
+                    new ConcurrentDictionary<string, TaskCompletionSource<string>>();
+
+        public UploadService()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                if (!callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out TaskCompletionSource<string> tcs))
+                    return;
+                var body = ea.Body.ToArray();
+                var response = Encoding.UTF8.GetString(body);
+                Console.WriteLine(response);
+                tcs.TrySetResult(response);
+            };
+        }
+
+        public Task CallAsync(File file, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            IBasicProperties props = channel.CreateBasicProperties();
+            var message = new { file.Email, file.Name, file.FileFormat };
+            var correlationId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = replyQueueName;
+            var messageBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            var tcs = new TaskCompletionSource<string>();
+            callbackMapper.TryAdd(correlationId, tcs);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: file.FileFormat,
+                basicProperties: props,
+                body: messageBytes);
+
+            channel.BasicConsume(
+                consumer: consumer,
+                queue: replyQueueName,
+                autoAck: true);
+
+            cancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out var tmp));
+            return tcs.Task;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
